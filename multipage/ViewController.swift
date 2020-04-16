80980cb934960b007da1fab2d3831f276318f5a5
yinoue <yinoue@scala-com.jp>
2020-04-16 16:22:10 +0900

    Initial Commit

---
+++ multipage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multipage
+//
+//  Created by 井上義章 on 20200416.
+//  Copyright © 2020 井上義章. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
