1340a050f4dc3adace2364a33ae6517fcf6bc530
yinoue <yinoue@scala-com.jp>
2020-04-23 11:36:48 +0900

    autofocus

--- multipage/ViewController.swift
+++ multipage/ViewController.swift
@@ -124,13 +124,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var memoListTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // メモリスト
         memoListTable.frame = view.frame
         memoListTable.dataSource = self
         memoListTable.delegate = self
         memoListTable.tableFooterView = UIView(frame: .zero)
         memoListTable.allowsMultipleSelectionDuringEditing = true
-        navigationItem.rightBarButtonItem = editButtonItem
+        // 追加用テキストフィールド
         newTitleField.delegate = self
+        newTitleField.becomeFirstResponder() // 選択
+        // ヘッダーメニュー
+        navigationItem.rightBarButtonItem = editButtonItem
         hideKeyboardWhenTappedAround()
     }
     override func viewDidAppear(_ animated: Bool) {
@@ -153,9 +158,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewWillAppear(animated)
         configureObserver()  //Notification発行
     }
-
-    // MARK: - Notification
-
     /// Notification発行
     func configureObserver() {
         let notification = NotificationCenter.default
@@ -163,32 +165,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                                  name: UIResponder.keyboardWillShowNotification, object: nil)
         notification.addObserver(self, selector: #selector(keyboardWillHide(_:)),
                                  name: UIResponder.keyboardWillHideNotification, object: nil)
-        print("Notificationを発行")
     }
 
-    /// キーボードが表示時に画面をずらす。
+    // キーボードが表示時に画面をずらす。
     @objc func keyboardWillShow(_ notification: Notification?) {
         guard let rect = (notification?.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue,
-            let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        let duration = notification?.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
         UIView.animate(withDuration: duration) {
             let transform = CGAffineTransform(translationX: 0, y: -(rect.size.height))
             self.view.transform = transform
         }
-        print("keyboardWillShowを実行")
     }
 
-    /// キーボードが降りたら画面を戻す
+    // キーボードが降りたら画面を戻す
     @objc func keyboardWillHide(_ notification: Notification?) {
         guard let duration = notification?.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? TimeInterval else { return }
         UIView.animate(withDuration: duration) {
             self.view.transform = CGAffineTransform.identity
         }
-        print("keyboardWillHideを実行")
     }
     
 }
 
 extension UIViewController {
+    // キーボード外をタップした時
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.hideKeyboard))
         tap.cancelsTouchesInView = false
